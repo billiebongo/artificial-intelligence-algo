051940f85a4f6e7f48cd4aa63a6a72e58ac12a08
sloth <jaslynlek@gmail.com>
2018-10-07 20:45:43 -0400

    fixed issue w rrecurson in version c

--- sudoku_main.py
+++ sudoku_main.py
@@ -1,7 +1,8 @@
 
 
 from sudoku_utils import *
-import Queue
+from queue import Queue
+import queue
 import copy
 
 fileDir = os.path.dirname(os.path.realpath('__file__'))
@@ -26,10 +27,10 @@ def do_forwardchecking(grid, i, j):
 
 	count = 0
 	# checking for row
-	for x in range(0, N):
+	for x in range(0, 9):
 		if grid[i][x] == 0:
 			for num1 in range(1, 81 + 1):
-				isValidMove(grid, x, j, num1):
+				if isValidMove(grid, x, j, num1):
 					count = count + 1
 
 					# Count 0 means this cell has now 0 legal values do return false
@@ -42,8 +43,9 @@ def do_forwardchecking(grid, i, j):
 	subGridStartRow = i - i % 3
 	subGridStartCol = j - j % 3
 
-	for x in range(0, 9):
-		for y in range(0, 9):
+	for x in range(0, 3):
+		for y in range(0, 3):
+
 			if grid[x + subGridStartRow][y + subGridStartCol] == 0:
 				for num1 in range(1, 81 + 1):
 					if isValidMove(grid,  x + subGridStartRow, y + subGridStartCol, num1):
@@ -60,12 +62,15 @@ def do_forwardchecking(grid, i, j):
 def get_lcv(grid, i, j):
 	# next value to assign to a var will be selected based on no of constraint it places on other var
 	# with the value causing the least number of constraints selected.
-	count = 0
+
 	lcv_queue = queue.PriorityQueue()
 	grid_copy = copy.deepcopy(grid)
 
-
+	prettyPrint(grid)
+	print(i,j)
 	for value in range(1, 10):
+		print("value {}".format(value))
+
 
 		#find all valid values for cell
 		if isValidMove(grid_copy, i, j, value):
@@ -75,17 +80,19 @@ def get_lcv(grid, i, j):
 			for x in range(0, 9):
 				if grid_copy[x][j] == 0:
 					for num1 in range(1, 10):
-						if isValidMove(grid1, x, j, num1):
+						if isValidMove(grid_copy, x, j, num1):
 							count = count + 1
+
+			print("count1:" + str(count))
 							# checking for row
 
 			# calc no of valid moves for each cell in row
 			for x in range(0, 9):
-				if grid1[i][x] == 0:
+				if grid_copy[i][x] == 0:
 					for num1 in range(1, 10):
-						if isValidMove(grid_copy, N, M, K, i, x, num1):
+						if isValidMove(grid_copy, i, x, num1):
 							count = count + 1
-
+			print("count2:" + str(count))
 			# to
 			subGridStartRow = i - i % 3
 			subGridStartCol = j - j % 3
@@ -93,54 +100,63 @@ def get_lcv(grid, i, j):
 			#calc no of valid moves for each cell the subgrid
 			for x in range(0, 3):
 				for y in range(0, 3):
-					if grid1[x + subGridStartRow][y + subGridStartCol] == 0:
+					if grid_copy[x + subGridStartRow][y + subGridStartCol] == 0:
 						for num1 in range(1, 10):
-							if isValidMove(grid1,  x + subGridStartRow, y + subGridStartCol, num1):
+							if isValidMove(grid_copy,  x + subGridStartRow, y + subGridStartCol, num1):
 								count = count + 1
-			lcv_queue.put(num, -1*count) # times -1 to make first value to have the highest counts/valid moves
+			print("count3"+str(count))
+			lcv_queue.put(value, -1*count) # times -1 to make first value to have the highest counts/valid moves
 	print(lcv_queue)
 	return lcv_queue
 
 def get_neighbors(grid, i, j):
+
 	neighbours = []
-	neighbours.append([(c, j) for c in range(9) if c != (i, j)])
-	neighbours.append([(i, c) for c in range(9) if c != (i, j)])
+	neighbours += [(c, j) for c in range(9) if c != (i, j)]
+	neighbours += [(i, c) for c in range(9) if c != (i, j)]
 	subGridStartRow = i - i % 3
 	subGridStartCol = j - j % 3
 	for x in range(0, 3):
 		for y in range(0, 3):
-			neighbours.append(subGridStartRow+x, subGridStartCol+y)
+			neighbours.append((subGridStartRow+x, subGridStartCol+y))
 	neighbours.remove((i, j))
 	print(neighbours)
-	return neighbors
+	return neighbours
 
 
 
-def get_degree(grid):
+def get_degree(grid, min_count_cells):
 	# the next var to be assigned will tbe the value which is involved in the most
 	# no of constraints w other var
 	# If a tie (such as choosing the start state), choose the variable involved in the most
-	# constraints
-	# this is to reduce branching factor, since fewer legal successors of that node
+	# constraints, this is to reduce branching factor, since fewer legal successors of that node
 
 	max_unassigned_neighbours = 0
 
 	# for each cell, get calc the degree
-	for i in range(0, 9):
-		for j in range(0, 9):
-			num_unassigned_neighbours = 0
-			var_neighbours = get_neighbors(grid,i,j)
+	print("find degrees for")
+	print(min_count_cells)
+	for cell in min_count_cells:
+		print(cell)
+		num_unassigned_neighbours = 0
+		var_neighbours = get_neighbors(grid,cell[0],cell[1])
 
-			no_of_unassigned_var = 0
-			for var in var_neighbours:
-				i, j = var
-				if grid[i][j] == 0:
-					numUnassignedNeighbors += 1
-			if num_unassignedNeighbors >max_unassigned_neighbours:
-				degree_i = i
-				degree_j = j
-				max_unassigned_neighbours = num_unassigned_neighbors
+		no_of_unassigned_var = 0
+		for var in var_neighbours:
 
+			i, j = var[0], var[1]
+			if grid[i][j] == 0:
+				num_unassigned_neighbours += 1
+		print("num of unass neighbouts {}".format(num_unassigned_neighbours))
+		if num_unassigned_neighbours >max_unassigned_neighbours:
+			#does not consider ties, just takes last one with count == maxcoutn
+			print("max found!")
+			print(cell)
+			degree_i = cell[0]
+			degree_j = cell[1]
+			max_unassigned_neighbours = num_unassigned_neighbours
+	print("returning...")
+	print(degree_i, degree_j)
 	return degree_i, degree_j
 
 
@@ -149,56 +165,51 @@ def get_degree(grid):
 def get_MRV(grid):
 	count = 0
 	min_count = 82 # can be 81?
-
-	# init mrv square to be top left square
-	mrv_i = 0
-	mrv_j = 0
 	flag = False
 
-	all_counts = []
-
-
+	min_count_cells = []
 	for i in range(0, 9):
 		for j in range(0, 9):
-
 			# get RV from each non-filled square
 			if grid[i][j] == 0:
 				flag = True
 				count = 0
-				for num in range(1, 9):
+				for num in range(1, 10):
 					if isValidMove(grid, i, j, num):
 						count = count + 1
 				# If current cell has mim minimum possible legal
 				# values than our previous count,
 				# update global count and update mrv cell values
 
-				all_counts.append(count) # position of element in count will tell the cell
+				#keep track of cells with the min_count. need tp refresh when a new min_count found
+
+				if count ==  min_count:
+					min_count_cells.append([i,j])
 
 				if count < min_count:
 
 					# keep track of all counts
-
-
+					min_count_cells = []
+					min_count_cells.append([i,j])
 					min_count = count
-					mrv_i = i
-					mrv_j = j
+
 
 	#if flag == False:  # can't find any empty cell in sudoku board
 	#    return -1, -1
-	mrv_list = []
-	for index, count in enumerate(all_counts):
-		if count == min_count:
-			mrv_list.append(index)
 
 
-
-	# TODO if there is a tie value, use degree heuristics
-	# there is a tie
-	if len(mrv_list) >1:
-		return -1, -1
+	print("min count cells is ")
+	print(min_count_cells)
+	if len(min_count_cells) >1:
+		return True, min_count_cells # list of 2-element lists
 	# there is no tie
+	elif len(min_count_cells) == 1:
+		return  False, min_count_cells # list of 1 2-element list
 	else:
-		return mrv_i, mrv_j
+		print("Error, min count cells cant be 0")
+		raise
+
+
 
 
 
@@ -208,13 +219,16 @@ def get_MRV(grid):
 #removed from the domain of the free variables that are either in the same line, in the
 #same column or in the same square as the assigned variable
 def solveBacktrackingFWD(grid):
-	steps = 0
 	prettyPrint(grid)
+	#prettyPrint(grid)
 
 	# Get a unassigned loaction from the grid
 	i, j = findUnassignedLocation(grid)
-
+	steps = 0
 	if steps >10000:
+		print("overshot 10000")
+
+		prettyPrint(grid)
 		return grid, steps
 
 	if i == -1:
@@ -228,7 +242,7 @@ def solveBacktrackingFWD(grid):
 
 
 			# Recursive Call
-			if solveBacktracking(grid)[0]: #return False to call for backtracking!!!
+			if solveBacktrackingFWD(grid)[0]: #return False to call for backtracking!!!
 				return True, steps
 
 			# Backtrack all the way until hit solvebacktracing -- true
@@ -244,7 +258,7 @@ def solveBacktrackingFWD(grid):
 
 # Function to solve sudoku by back tracking
 def solveBacktracking(grid):
-	steps = 0
+
 	prettyPrint(grid)
 
 	# Get a unassigned loaction from the grid
@@ -272,32 +286,109 @@ def solveBacktracking(grid):
 
 	return False, steps
 
-steps3 = 0
 
-# Solve Sudoku using Backtracking + MRV + LCV + Forward Checking + degree
-def solveSudokuBacktrackingMRVDegreefwdLCV(grid, N, M, K):
+def get_MRV2(grid):
+	count = 0
+	global_count = 82
+	mrv_i = 0
+	mrv_j = 0
+	flag = False
 
+	for i in range(0, 9):
+		for j in range(0, 9):
+			if grid[i][j] == 0:
+				flag = True
+				count = 0
+				for num in range(1, 10):
+					if isValidMove(grid, i, j, num):
+						count = count + 1
+				# If current cell has mim minimum possible legal values than our previous count, update global count and update mrv cell values
+				if count < global_count:
+					global_count = count
+					mrv_i = i
+					mrv_j = j
+
+	if flag == False:  # can't find any empty cell in sudoku board
+		return -1, -1
+
+
+	return mrv_i, mrv_j
+
+# Solve Sudoku using Backtracking + MRV + LCV + Forward Checking
+def solveSudokuBacktrackingMRVfwd(grid):
+	global consistencyChecks3
 	lcv_queue = queue.PriorityQueue()
 	# Game over check, if sudoku is solved fully or not
-	i, j = findUnassignedLocation(grid, N)
+	i, j = findUnassignedLocation(grid)
 	if i == -1:
-		return True, steps3
+		prettyPrint(grid)
+		return True, consistencyChecks3
 
 	# Get MRV cell from the grid
-	i, j = get_MRV(grid)
-	if i == -1:
-		i, j = get_degree(grid)
+	i, j = get_MRV2(grid)
 
-	# Get set of lcv values for chosen var cell
-	lcv_queue = get_LCV(grid,  i, j)
+	# Get set of lcv values for mrv cell
+	lcv_queue = get_lcv(grid, i, j)
 
-	# find MRV then apply LCV to choose possible values with priority
+	# Check by putting all lcv_values in MRV cell one by one and try to solve sudoku by backtracking and forward checking
 	while not lcv_queue.empty():
 		lcv_value = lcv_queue.get()
 		if isValidMove(grid, i, j, lcv_value):
 			grid[i][j] = lcv_value
+			consistencyChecks3=3
+			consistencyChecks3 = consistencyChecks3 + 1
 
-			steps3 = step3 + 1
+			# Do forward checking after assigning value to the cell
+			fwdcheckingresult = do_forwardchecking(grid,  i, j)
+
+			if fwdcheckingresult == True:
+				if solveSudokuBacktrackingMRVfwd(grid)[0]:
+					return True, consistencyChecks3
+			else:
+				grid[i][j] = 0
+
+
+	return False, consistencyChecks3
+# Solve Sudoku using Backtracking + MRV + LCV + Forward Checking + degree
+def solveSudokuBacktrackingMRVDegreefwdLCV(grid):
+
+	lcv_queue = queue.PriorityQueue()
+	# Game over check, if sudoku is solved fully or not
+	i, j = findUnassignedLocation(grid)
+	steps3=0
+	# done
+	if i == -1:
+		prettyPrint(grid)
+		return True, steps3
+
+
+	# Get MRV cell from the grid
+	tie, min_count_cells = get_MRV(grid)
+	print("min count cells are")
+	print(tie)
+	print(min_count_cells)
+	if tie == True:
+		i, j = get_degree(grid, min_count_cells)
+		#need to make sure only return one set? no ties
+	else:
+		print("YAP")
+		i, j = min_count_cells[0][0], min_count_cells[0][1]
+
+	# Get set of lcv values for chosen var cell
+
+	lcv_queue = get_lcv(grid,  i, j)
+
+
+	print(lcv_queue.empty())
+	print("lcv queue empty causes term")
+	# find MRV then apply LCV to choose possible values with priority
+	while not lcv_queue.empty():
+		lcv_value = lcv_queue.get()
+		print(lcv_value)
+		if isValidMove(grid, i, j, lcv_value):
+			grid[i][j] = lcv_value
+			steps3=0
+			steps3 += 1
 
 			# Do forward checking after assigning value to the cell
 			fwdcheckingresult = do_forwardchecking(grid, i, j)
@@ -308,25 +399,50 @@ def solveSudokuBacktrackingMRVDegreefwdLCV(grid, N, M, K):
 			else:
 				grid[i][j] = 0
 
+	steps3 = 2132
+	print("oops")
 	return False, steps3
 
 
-def runAll():
+def runAll(version):
+	if version == 'A':
 
-	for x in range(1, 72):
-		for y in range(1, 11):
-			grid = getGrid(os.path.join(fileDir, 'data/sudoku_problems/{}/{}.sd'.format(x, y)))
-			boo, steps = solveBacktracking(grid)
-			print(grid, steps)
+
+		for x in range(1, 72):
+			for y in range(1, 11):
+				grid = getGrid(os.path.join(fileDir, 'data/sudoku_problems/{}/{}.sd'.format(x, y)))
+				boo, steps = solveBacktracking(grid)
+				prettyPrint(grid)
+	if version == 'B':
+		for x in range(52, 58):
+			for y in range(3, 5):
+				grid = getGrid(os.path.join(fileDir, 'data/sudoku_problems/{}/{}.sd'.format(x, y)))
+				boo, steps = solveBacktrackingFWD(grid)
+				prettyPrint(grid)
+	if version == 'C':
+		for x in range(57, 58):
+			for y in range(4, 5):
+				grid = getGrid(os.path.join(fileDir, 'data/sudoku_problems/{}/{}.sd'.format(x, y)))
+				boo, steps = solveSudokuBacktrackingMRVDegreefwdLCV(grid)
+				print(grid, steps)
+
 
 def runOne(x, y, version):
 	grid = getGrid(os.path.join(fileDir, 'data/sudoku_problems/{}/{}.sd'.format(x, y)))
 	if version == 'A':
 		boo, steps = solveBacktracking(grid)
 	if version == 'B':
-		boo, steps = solveBacktracking(grid)
+		prettyPrint(grid)
+		boo, steps = solveBacktrackingFWD(grid)
 	if version == 'C':
-		boo, steps = solveBacktracking(grid)
+		steps3 = 0
+
+
+
+		boo, steps3 =  solveSudokuBacktrackingMRVDegreefwdLCV(grid)
+	if version == 'D': #test
+		boo, steps3 = solveSudokuBacktrackingMRVfwd(grid)
+
 
 if __name__ == "__main__":
 
@@ -334,4 +450,5 @@ if __name__ == "__main__":
 
 	# 2bd: implement switch to choose between suing LCV or the other first
 
-	runOne(36,8, 'B')
+	runOne(36,8, 'C')
+	#runAll('B')
