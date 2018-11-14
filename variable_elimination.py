f8529f290a02ac9932c7bdbfffadffc88f6555e7
sloth <jaslynlek@gmail.com>
2018-11-14 10:24:32 -0500

    input test results fixed

--- variable_elimination.py
+++ variable_elimination.py
@@ -23,11 +23,14 @@ def restrict(factor, variable, value):
     #for i in range(variable):
     #    factor[]
     #return Factor(,factor.array.take(value, variable))
-
+    print(factor.__dict__)
     var_index = factor.variables.index(variable)
     restricted_arr = np.take(factor.array, value, axis=var_index)
-    restricted_var = copy.deepcopy(factor.variables).pop(var_index)
+    restricted_var = copy.deepcopy(factor.variables)
+    restricted_var.remove(variable)
     restricted_factor = Factor(restricted_var, restricted_arr)
+    print(restricted_factor.__dict__)
+    print("RESTR")
     return restricted_factor
 
 def sum_out(factor, variable):
@@ -49,28 +52,31 @@ def sum_out(factor, variable):
 
 def multiply(factor1, factor2):
     """ merge on common variables. for one common var ONLY """
-
+    print("MULTIPLYING")
+    print(factor1.variables)
+    print(factor2.variables)
+    print(factor1.array)
+    print(factor2.array)
     copy1 = copy.deepcopy(factor1)
     copy2 = copy.deepcopy(factor2)
-    print(type(factor1.variables))
-    print(factor1.__dict__)
+
     combined_var = factor1.variables + list(set(factor2.variables) - set(factor1.variables))
     sorted_var = sorted(combined_var)
     for var in sorted_var:
         if var not in factor1.variables:
-            print(var)
             copy1.variables.insert(sorted_var.index(var), var)
             copy1.array = np.expand_dims(copy1.array,
                                        axis=sorted_var.index(var))
         if var not in factor2.variables:
-            print(var)
+
             copy2.variables.insert(sorted_var.index(var), var)
             copy2.array = np.expand_dims(copy2.array,
                                        axis=sorted_var.index(var))
 
+
     product_arr = np.multiply(copy1.array, copy2.array)
     product_factor = Factor(sorted_var, product_arr)
-    print("TESINF PDT FCT")
+    print("MULTIPLYING RESULTS")
     print(product_factor.array)
     print(product_factor.variables)
     return product_factor
@@ -87,15 +93,21 @@ def normalizedFactor(factor):
 
 def resultFactor(factorList, queryVariables, orderedListOfHiddenVariables,EvidenceList):
 
-
-    for evidence in EvidenceList:
+    for f in factorList:
+        print(f.array)
+    for k in EvidenceList:
         #restrict
         print("no evide")
         for factor in factorList:
-            if evidence[0] in factor.variables:
-                new_factor=restrict(factor, evidence[0], evidence[1])
-                factorList.remove(factor)
+            if k in factor.variables:
+                factors_to_remove=[]
+                #factor, var, vALUE
+                new_factor = restrict(factor, k, EvidenceList[k])
+                factors_to_remove.append(factor)
+                new_factor.__dict__
                 factorList.append(new_factor)
+
+        factorList=[x for x in factorList if x not in factors_to_remove]
 
     for hiddenVariable in orderedListOfHiddenVariables:
         #find factors containing hiddenVariable
@@ -159,7 +171,7 @@ def resultFactor(factorList, queryVariables, orderedListOfHiddenVariables,Eviden
         print(f.variables)
         print(f.array)
 
-    print("HELLO")
+    print("HELLOBYE")
     new_factor =  factorList[0]
 
     for i in range(len(factorList) - 1):
@@ -183,18 +195,32 @@ f5=Factor(["M","NA", "NH"], np.array([[[1,0],[0.5,0.5]],[[0.6,0.4],[0.2, 0.8]]])
 f6=Factor(["FH","M",  "NH", "S"], np.array([[[[1,0.5],[0.8,0.25]],[[0.6,0.1],[0.35,0.01]]],
                                             [[[0,0.5],[0.2,0.75]],[[0.4,0.9],[0.65,0.99]]]]))
 
+#evidenceList is dictionary with var-value key value pair
 
-
-factorList=[f1,f2,f3,f4,f5,f6]
-queryVariables = {"FH":1}
-EvidenceList = {}
-orderedListofHiddenVariables = getOrderedListOfHiddenVariables(queryVariables.keys(), EvidenceList.keys())
-result=resultFactor(factorList,queryVariables,orderedListofHiddenVariables, EvidenceList)
-print("WHAT")
-print(result.variables, result.array)
+#factorList=[f1,f2,f3,f4,f5,f6]
+#queryVariables = {"FH":1}
+#EvidenceList = {}
+#orderedListofHiddenVariables = getOrderedListOfHiddenVariables(queryVariables.keys(), EvidenceList.keys())
+#result=resultFactor(factorList,queryVariables,orderedListofHiddenVariables, EvidenceList)
+##print("WHAT")
+#print(result.variables, result.array)
 #test=[[[0.32,0.48],[0.14,0.06]],[[0.36,0.54],[0.07,0.03]]]
 #n=sum_out(Factor(["A","B","C"], test), 'B')
 #print(n.array)
 #print(n.variables)
 
+
+t1=Factor(["E"],np.array([0.9997,0.0003]))
+t2=Factor(["B"],np.array([0.9999, 0.0001]))
+t3=Factor(["E","R"],np.array([[0.9998,0.0002],[0.1,0.9]]))
+t4=Factor(["A", "B", "E"],np.array([[[0.99,0.8],[0.05,0.04]],[[0.01,0.2],[0.95,0.96]]]))
+t5=Factor(["A","W"],np.array([[0.6,0.4],[0.2,0.8]]))
+
+t6=Factor(["A","G"],np.array([[0.96,0.04],[0.6,0.4]]))
+EvidenceList={"W":1,"G":1}
+queryVariables={"B":1}
+factorList=[t1,t2,t3,t4,t5,t6]
+orderedListofHiddenVariables=["R","E","A"]
+result=resultFactor(factorList,queryVariables,orderedListofHiddenVariables, EvidenceList)
+print(result.__dict__)
 
