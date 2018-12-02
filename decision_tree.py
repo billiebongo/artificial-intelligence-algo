6ee67ab14ed6184fbd7c309740dce63f74ae3647
sloth <jaslynlek@gmail.com>
2018-12-02 00:11:31 -0500

    decision tree skeleton done

--- decision_tree.py
+++ decision_tree.py
@@ -4,196 +4,205 @@ import pandas as pd
 
 
 class DecisionTree():
-    tree = {}
+	tree = {}
 
-    def learn(self, training_set, features, target):
-        self.tree = ID3(features, examples)
+	def learn(self, training_set, features, target):
+		self.tree = ID3(features, examples)
 
 
 # Class Node which will be used while classify a test-instance using the tree which was built earlier
 class Node():
-    value = "" # lower or higher
-    children = []
+	value = "" # lower or higher
+	children = []
 
-    def __init__(self, val, dictionary):
-        self.value = val
-        if (isinstance(dictionary, dict)):
-            self.children = dictionary.keys()
+	def __init__(self, class, children):
+		self.class = class
+		if (isinstance(dictionary, dict)):
+			self.children = children
 
-def print_tree(tree):
-    return
 
-def find_split_point():
-    #split point is chosen based on max gain in entropy
 
-    return_split_point
-
+def get_f_index(feature):
+	return ['sepal_length', 'sepal_width' , 'petal_length', 'petal_width'].index(feature)
 
 # Majority Function which tells which class has more entries in given data-set
-def choose_majority_class(features, data, target):
-    freq = {}
-    index = features.index(target)
+def choose_majority_class(examples, features):
+	freq = {}
 
-    for tuple in data:
-        if (freq.has_key(tuple[index])):
-            freq[tuple[index]] += 1
-        else:
-            freq[tuple[index]] = 1
+	for row in examples:
+		if (freq.has_key(row[-1])):
+			freq[row] += 1
+		else:
+			freq[row] = 1
 
-    max = 0
-    major = ""
+	majority_class = max(freq, key=freq.get)
 
-    for key in freq.keys():
-        if freq[key] > max:
-            max = freq[key]
-            major = key
-
-    return major
+	return majority_class
 
 
 # Calculates the entropy of the data given the target attribute
-def calc_entropy(features, data, targetAttr):
-    """ features """
-    freq = {}
-    entropy = 0.0
-
-    i = 0
-    for feature in features:
-        if (targetAttr == features):
-            break
-        i = i + 1
-
-    i = i - 1
-
-    #classifying the datra
-    for row in data:
-        if (freq.has_key(row[i])):
-            freq[entry[i]] += 1.0
-        else:
-            freq[entry[i]] = 1.0
-
-    for prob in freq.values():
-        entropy += (-prob/ len(data)) * math.log(prob / len(data), 2)
-
-    return entropy
+def calc_entropy(c0_count, c1_count, c2_count):
+	""" Assuminh 3 classes """
+	total=c1_count+c2_count+c3_count
+	#classifying the datra
+	#for row in data:
+	#    if (freq.has_key(row[i])):
+	#        freq[entry[i]] += 1.0
+	#    else:
+	#        freq[entry[i]] = 1.0
 
 
-def get_best_split_point(data, index_of_attribute_to_sort_by):
-    #sort the list of lists by value at index of inner list
-    index_of_attribute_to_sort_by =
+	entropy = (-c0_count/ total) * math.log(c0_count / total, 2) + (-c1_count/ total) * math.log(c1_count/ total, 2) + (-c2_count/ total) * math.log(c2_count/ total, 2)
 
-    sorted_data=sorted(data, key=lambda row: row[4])
+	return entropy
 
-    for row in data:
-
-
-    return possible_split_points
+def calc_info_gain(current_entropy, splitting_point, feature,examples ):
+	"""split the examples and return the info gain"""
 
 
-# Calculates the information gain (reduction in entropy) in the data when a particular attribute is chosen for splitting the data.
-def info_gain(features, data, attr, targetAttr):
+	f_index = get_f_index(feature)
+
+	after_class_entropy = 0
+	for i in range(2):
+		if i==0:
+			#less than
+
+			subset_examples = [e for e in examples if e[f_index]<splitting_point]
+			prob = len(subset_examples) / len(examples)
+		else:
+			#more than
+
+			subset_examples = [e for e in examples if e[f_index] > splitting_point]
+			prob = len(subset_examples) / len(examples)
+		c0_count = 0
+		c1_count = 0
+		c2_count = 0
+		for row in subset_examples:
+			if (str(row[-1])==str('0.0')):
+				c0_count +=1
+
+			if str(row[-1])==str('1.0'):
+				c1_count +=1
+
+			if str(row[-1])==str('2.0'):
+				c2_count +=1
+		after_class_entropy +=  prob* calc_entropy(c0_count, c1_count, c2_count)
+
+	info_gain = current_entropy - after_class_entropy
+	return info_gain
 
 
-    freq = {}
-    after_class_entropy = 0.0
-    i = features.index(attr)
+def get_info_gain_for_feature(f, examples):
+	""" find best split point for feature """
+	f_index=get_f_index(f)
+	# sort by valye of that feature
+	sorted_data = sorted(data, key=lambda row: row[f_index])
+	possible_split_points=[]
+	prev_row_class=examples[0][-1]
+	for row in examples:
+		if row[-1] != prev_row_class:
+			possible_split_points.append((row[f_index]+prev_row_f_value)/2)
+		prev_row_f_value=row[f_index]
+	max_possible_split_info_gain_value = 0
+	max_possible_splitting_point= 0
 
-    # sorts rows in data acc to the value in the col of attr
-    for f_index in range(len(features)):
-        best_split_point = get_possible_split_points(data, )
+	# FINDING THE BEST SPLIT POINT
 
-    for row in data:
-        if (freq.has_key(row[i])):
-            freq[row[i]] += 1.0
-        else:
-            freq[row[i]] = 1.0
+	#current
+	c0= len([e for e in examples if str(e[-1])==str('0.0')])
+	c1= len([e for e in examples if str(e[-1])==str('1.0')])
+	c2= len([e for e in examples if str(e[-1])==str('2.0')])
+	curr_entropy = calc_entropy()
 
-    for val in freq.keys():
-        prob = freq[val] / sum(freq.values())
-        dataSubset = [entry for entry in data if entry[i] == val]
-        after_class_entropy += prob * calc_entropy(features, dataSubset, targetAttr)
-    info_gain = entropy(features, data, targetAttr) - after_class_entropy
-    return info_gain
+	for possible_split_point in possible_split_points:
+		info_gain = calc_info_gain(current_entropy, possible_splitting_point, f,examples )
+		if info_gain>max_possible_split:
+			max_possible_splitting_point= possible_split_point
+			max_possible_split_info_gain_value = info_gain
+
+	return max_possible_split_info_gain_value, max_possible_splitting_point
+
+
+
 
 
 
 
 # This function chooses the attribute among the remaining features which has the maximum information gain.
-def choose_best_feature(data, features, target):
-    best = features[0]
-    maxGain = 0;
+def choose_best_feature(examples, features, current_entropy):
+	best_feature = features[0]
+	max_info_gain = 0
+	best_feature_splitting_point=0
 
-    for attr in features:
-        #for each feature order the values for that feature.
-        newGain = info_gain(features, data, attr, target)
-        if newGain > maxGain:
-            maxGain = newGain
-            best = attr
+	# find best feature and respective splitting point
+	for f in features:
 
-    return best
+
+		# i need thge feature and their respective info_gain
+		new_info_gain, splitting_point = get_info_gain_for_feature(f, features, examples)
+
+		if new_info_gain > max_info_gain:
+			max_info_gain = new_info_gain
+			best_feature_splitting_point=splitting_point
+            best_feature=f
+
+
+	return best_feature, splitting_point
 
 
 # This function will get unique values for that particular attribute from the given data
 def get_values(data, features, attr):
-    index = features.index(attr)
-    values = []
+	index = features.index(attr)
+	values = []
 
-    for entry in data:
-        if entry[index] not in values:
-            values.append(entry[index])
+	for entry in data:
+		if entry[index] not in values:
+			values.append(entry[index])
 
-    return values
+	return values
 
 
-# This function will get all the rows of the data where the chosen "best" attribute has a value "val"
-def get_data(data, features, best, val):
-    new_data = [[]]
-    index = features.index(best)
-
-    for entry in data:
-        if (entry[index] == val):
-            newEntry = []
-            for i in range(0, len(entry)):
-                if (i != index):
-                    newEntry.append(entry[i])
-            new_data.append(newEntry)
-
-    new_data.remove([])
-    return new_data
+def split_examples(examples, f, splitting_point):
 
 
 # This function is used to build the decision tree using the given data, features and the target features. It returns the decision tree in the end.
 def ID3(examples, features):
 
-    examples= examples[:]
+	examples= examples[:]
 
-    # list of all classes of data
-    classes = [row[features.index(target)] for row in data]
+	# list of all classes of data
+	classes = [row[features.index(target)] for row in data]
 
-    #if all in the same class
-    if len(set(classes)):
-        print("all of the same class")
-        return classes[0]
+	#if all in the same class
+	if len(set(classes))==1: #classes.count(classes[0]) == len(classes):
+		print("all of the same class")
+		return Node(classes[0], examples)
 
-    #if no more examples or features
-    if not data or (len(features) - 1) <= 0:
-        return default
-    #if all data of the same class
-    elif classes.count(classes[0]) == len(classes):
-        return classes[0]
-    #
-    else:
-        best = choose_best_feature(data, features, target)
-        tree = {best: {}}
+	#if no more features, choose majority function
+	elif (len(features) - 1) <= 0:
+		return Node(choose_majority_class(examples, features), examples)
+	#if no more examples, but still got features, return leaf node with majority decision of parent node
+	elif len(examples)==0:
+		return Node(choose_parent_class, examples)
 
-        for val in get_values(data, features, best):
-            new_data = get_data(data, features, best, val)
-            newAttr = features[:]
-            newAttr.remove(best)
-            subtree = build_tree(new_data, newAttr, target)
-            tree[best][val] = subtree
+	else:
+        best_feature, splitting_point=choose_best_feature(examples, features, current_entropy)
+        #split examples with spliting point
+        arc = split_examples(examples, best_feature, splitting_point)
 
-    return tree
+	#
+	else:
+		best = choose_best_feature(data, features, target)
+		tree = {best: {}}
+
+		for val in get_values(data, features, best):
+			new_data = get_data(data, features, best, val)
+			newAttr = features[:]
+			newAttr.remove(best)
+			subtree = ID3(new_data, newAttr, target)
+			tree[best][val] = subtree
+
+	return tree
 
 import csv
 # This function runs the decision tree algorithm. It parses the file for the data-set, and then it runs the 10-fold cross-validation. It also classifies a test-instance and later compute the average accurracy
@@ -201,26 +210,26 @@ import csv
 # 1. Discrete Splitting for features "age" and "fnlwght"
 # 2. Random-ness: Random Shuffle of the data before Cross-Validation
 def run_decision_tree():
-    data = []
-    with open("data/set_a.csv") as csvfile:
+	data = []
+	with open("data/set_a.csv") as csvfile:
 
-        for line in csv.reader(csvfile, delimiter=","):
-            print(type(line))
-            data.append(line)
+		for line in csv.reader(csvfile, delimiter=","):
+			print(type(line))
+			data.append(line)
 
-    features = ['sepal_length', 'sepal_width' , 'petal_length', 'petal_width', 'class']
-    target = features[-1]
+	features = ['sepal_length', 'sepal_width' , 'petal_length', 'petal_width', 'class']
+	target = features[-1]
 
-    #for k in range(K):
-        #random.shuffle(data)
-        #training_set = [x for i, x in enumerate(data) if i % K != k]
-        #test_set = [x for i, x in enumerate(data) if i % K == k]
-    training_set=data
-    tree = DecisionTree()
-    tree.learn(training_set, features, target)
+	#for k in range(K):
+		#random.shuffle(data)
+		#training_set = [x for i, x in enumerate(data) if i % K != k]
+		#test_set = [x for i, x in enumerate(data) if i % K == k]
+	training_set=data
+	tree = DecisionTree()
+	tree.learn(training_set, features, target)
 
 
 if __name__ == "__main__":
-    run_decision_tree()
+	run_decision_tree()
 
 
